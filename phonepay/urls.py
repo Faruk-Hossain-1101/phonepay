2780cda1f69a44bc0ae3ed6fb62e58d01c724c54
Faruk Hossain <hossainf114@gmail.com>
2024-07-25 10:44:23 +0530

    first commit

---
+++ phonepay/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'payment'
+
+urlpatterns = [
+    path('checkout', views.checkout, name='checkout'),
+    path('payment-initiate', views.initiate_payment, name='initiate_payment'),
+    path('callback', views.payment_callback, name='callback'),
+]
